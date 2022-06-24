3042b8b169e354294f5fa271cd581021437fdab1
Semyon Baykov <simon.baykov@gmail.com>
2022-06-24 19:14:02 -0400

    Continuing work - minor changes

--- Collatz Conjecture.py
+++ Collatz Conjecture.py
@@ -1,4 +1,6 @@
 from math import sqrt
+import random
+import sys
 import pygame
 pygame.init()
 clock = pygame.time.Clock()
@@ -8,6 +10,7 @@ clock = pygame.time.Clock()
 bColor = (44, 47, 51)
 blColor = (114, 137, 218)
 rColor = (230, 230, 230)
+gColor = (255, 255, 255)
 
 bfont = pygame.font.Font(None, 18)
 
@@ -19,24 +22,23 @@ else: sFont2 = pygame.font.SysFont('Arial', 24)
 
 
 # Vars used in game
-numT = 'Start typing an integer...'
+numT = 'Type a positive integer...'
 num = 1
 collided = False
 wrongNum1 = False
-wrongNum2 = False
 
 
 # Screen
-screen = pygame.display.set_mode((800, 1000))
+screen = pygame.display.set_mode((800, 875))
 pygame.display.set_caption('Collatz Conjecture')
 screen.fill(bColor)
 
 
 # Text setup
-titleText = sFont1.render('Welcome to the Collatz Conjecture visualizer, input the starting number:', False, (255, 255,
-                                                                                                              255))
-input_a = pygame.Rect((30, 80), (745, 40))
-mButton = pygame.Rect((60, 130), (100, 40))
+input_a = pygame.Rect((30, 30), (745, 40))
+mButton = pygame.Rect((675, 30), (100, 40))
+gRect = pygame.Rect((45, 80), (705, 705))
+opt1 = pygame.Rect((50, 720), (100, 40))
 
 
 # Functions
@@ -47,6 +49,7 @@ def isNum(char):
     for i in range(1073741913, 1073741922):
         if char == i: return True
     return False
+
 # Take int and distribute accordingly
 def startCalc(num):
     # List the conjecture
@@ -60,12 +63,11 @@ def startCalc(num):
         else:
             num = 3 * num + 1
             numList.append(num)
-
     # Find stats
-    primeList = []
+    primeList = list()
     for i in numList:
-        primeList.append(isPrime(numList[i]))
-    print(primeList)
+        primeList.append(bool(isPrime(i)))
+
 # Check if prime
 def isPrime(num):
     if num == 1: return True
@@ -78,14 +80,16 @@ def isPrime(num):
 
 pygame.display.flip()
 while True:
-    # Title
-    screen.blit(titleText, (30, 42))
     # Input box
     pygame.draw.rect(screen, rColor, input_a)
-    screen.blit((sFont2.render(numT, False, (0, 0, 0))), (35, 85))
+    screen.blit((sFont2.render(numT, False, (0, 0, 0))), (35, 35))
     # Main Button
     pygame.draw.rect(screen, blColor, mButton)
-    screen.blit((sFont2.render('Visualize!', False, (0, 0, 0))), (69, 135))
+    screen.blit((sFont2.render('Enter', False, (0, 0, 0))), (704, 35))
+    # Graph
+    pygame.draw.rect(screen, gColor, gRect)
+    # Option buttons
+    pygame.draw.rect(screen, blColor, opt1)
 
 
     for event in pygame.event.get():
@@ -106,14 +110,9 @@ while True:
             # Button
             if mButton.collidepoint(event.pos):
                 blColor = (124, 137, 228)
-                if numT == 'Start typing an integer...':
-                    wrongNum1 = True
-                elif int(numT) > 9223372036854775807:
-                    wrongNum2 = True
-                else:
-                    wrongNum1 = False
-                    wrongNum2 = False
-                    startCalc(int(numT))
+                if numT == 'Type a positive integer...' or numT == '':
+                    numT = random.randint(1, sys.maxsize)
+                startCalc(numT)
             else:
                 blColor = (114, 137, 218)
         # Type
@@ -127,8 +126,6 @@ while True:
 
         # Misc events
         if wrongNum1: screen.blit((sFont2.render('< Please type an integer first!', False, (200, 0, 0))), (169, 135))
-        else: screen.blit((sFont2.render('', False, (200, 0, 0))), (169, 135))
-        if wrongNum2: text2 = screen.blit((sFont2.render('< Integer is too big!', False, (200, 0, 0))), (169, 135))
         else: screen.blit((sFont2.render('', False, (200, 0, 0))), (169, 135))
 
         # Clock
